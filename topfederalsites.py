db7f1d440b8b8d504ae105a40ead93e28e97815e
stvnrlly <steven.reilly@gsa.gov>
2016-06-19 23:48:11 -0400

    run every minute

--- topfederalsites.py
+++ topfederalsites.py
@@ -1,4 +1,4 @@
-import requests, psycopg2, tweepy, os, json, datetime
+import requests, psycopg2, tweepy, os, json, datetime, time
 from urllib.parse import urlparse
 
 # Get credits from a local file
@@ -28,23 +28,29 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS sites (url varchar, title varchar, visitors int, time timestamp);")
 
-# Check the data
-now_sites = requests.get("https://analytics.usa.gov/data/live/top-pages-realtime.json").json()
-cur.execute("SELECT url FROM sites;")
-old_sites = cur.fetchall()
-for site in now_sites["data"]:
-    if (site["page"],) in old_sites:
-        pass
-    else:
-        print("new site: "+site["page"])
-        time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-        cur.execute("INSERT INTO sites (url, title, visitors, time) VALUES (%s, %s, %s, %s)",(site["page"],site["page_title"],str(site["active_visitors"]), time))
-        if len(site["page_title"]) > 59:
-            title = site["page_title"][0:58]+u'\u2026'
+def check_data():
+    # Check the data
+    now_sites = requests.get("https://analytics.usa.gov/data/live/top-pages-realtime.json").json()
+    cur.execute("SELECT url FROM sites;")
+    old_sites = cur.fetchall()
+    for site in now_sites["data"]:
+        if (site["page"],) in old_sites:
+            pass
         else:
-            title = site["page_title"]
-        # print("New site in the top 20! Welcome to %s (%s) with %s visitors" %\
-        #     (site["page"],title,site["active_visitors"]))
-        api.update_status("New site in the top 20! Welcome to %s (%s) with %s visitors" %\
-            (site["page"],title,site["active_visitors"]))
-conn.commit()
+            print("new site: "+site["page"])
+            time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+            cur.execute("INSERT INTO sites (url, title, visitors, time) VALUES (%s, %s, %s, %s)",(site["page"],site["page_title"],str(site["active_visitors"]), time))
+            if len(site["page_title"]) > 59:
+                title = site["page_title"][0:58]+u'\u2026'
+            else:
+                title = site["page_title"]
+            # print("New site in the top 20! Welcome to %s (%s) with %s visitors" %\
+            #     (site["page"],title,site["active_visitors"]))
+            api.update_status("New site in the top 20! Welcome to %s (%s) with %s visitors" %\
+                (site["page"],title,site["active_visitors"]))
+    conn.commit()
+
+if __name__ == "__main__":
+    while True:
+        check_data()
+        time.sleep(60)
